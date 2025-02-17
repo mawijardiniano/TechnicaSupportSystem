b457adf9a7114dd58c9746aa53e6ee07b771d3d6
Mawi <jardinianomarklouisse@gmail.com>
2025-02-17 15:31:07 +0800

    create a admin dashboard

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const authenticateToken = require("../middleware/authentication");
-const { userLogin, userSignup } = require("../controllers/userController");
+const { userLogin, userSignup,getUsers } = require("../controllers/userController");
 
-
+router.get("/get-users", getUsers)
 router.post("/signup", userSignup);
 router.post("/login", userLogin);
 
